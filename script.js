9cffceff2a3f4b4fc739b55c56ac99edeb4625c6
Lenka Koubková <lvseteckova@gmail.com>
2025-04-18 10:52:15 +0200

    přidání html a js

---
+++ script.js
@@ -0,0 +1,29 @@
+/*Vypočítat výplatu včetně daně 15%
+
+document.body.innerHTML += 7 * 320 * 21;//měsíční příjem
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 7 * 320 * 21 - 7 * 320 * 21 * 0.6;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += Math.floor(7 * 320 * 21 * (1 - 0.6) * 0.15);//daň*/
+
+/*Film 223 minut - rozdělit na hodiny a minuty
+document.body.innerHTML += Math.floor(223 / 60); počet celých hodin, zaokrouhlím směrem dolů
+document.body.innerHTML += ' : ';
+document.body.innerHTML += 223 % 60;//počet zbývajících minut, modulo, zbytek po dělení
+
+/*vložení emailu do JS code
+document.body.innerHTML += 'lenka' + '.' + 'koubkova' + '@mujemail.com';*/
+
+/*document.body.innerHTML += '12' + 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 1 + 2 + 3 + '4';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '1' + 2 + 3 + 4;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '12' * 5;
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 12 * '5';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += '12' * '5';
+document.body.innerHTML += '<br>';
+document.body.innerHTML += 12 % '5';*/
